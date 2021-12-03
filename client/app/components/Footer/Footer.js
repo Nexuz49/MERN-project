2c586a890ef091b20791ceb06b2bec3dd49d6cd3
Nexuz49 <58955897+Nexuz49@users.noreply.github.com>
2021-12-03 15:17:16 +0100

    Local Repo to Git push
    -Repo was local on my machine. Pushing to github in chunks.

---
+++ client/app/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FooterScss from "./Footer.scss";
+
+const Footer = () => (
+  <footer>
+    <hr />
+    <p>MERN Project</p>
+    <p>
+      All rights and intended usages belong to the VELT Group Corp. IMT Limtd.
+      2021
+    </p>
+  </footer>
+);
+
+export default Footer;
---
+++ client/app/components/Header/Header.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import HeaderScss from "./Header.scss";
+
+import { Link, Route, Redirect } from "react-router-dom";
+
+// RouterGuard for hidden route after auth
+const ProtectedRoute = ({ component: Comp, loggedIn, path, ...rest }) => {
+  return (
+    <Route
+      path={path}
+      {...rest}
+      render={props => {
+        return loggedIn ? (
+          <Comp {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: {
+                prevLocation: path,
+                error: "Only users that are logged in can access this page!"
+              }
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+const Header = () => (
+  <header>
+    <nav>
+      <Link className="navAnchor1" to="/">
+        Home
+      </Link>
+
+      <Link className="navAnchor2" to="/helloworld">
+        About
+      </Link>
+    </nav>
+
+    <hr />
+  </header>
+);
+
+export default Header;
+
+// experimental/not done
+
+// class Header extends Component {
+//   state = {
+//     loggedIn: false
+//   };
+//   handleLogin = () => {
+//     const state = ({ state = {} } = this.props.location);
+//     const { prevLocation } = state;
+
+//     this.setState(
+//       {
+//         loggedIn: true
+//       },
+//       () => {
+//         this.props.history.push(prevLocation || "/");
+//       }
+//     );
+//   };
+//   render() {
+//     const state = ({ state = {} } = this.props.location);
+//     const { error } = state;
+//     return (
+//       <div>
+//         <div className="links">
+//           <Link to="/" className="link">
+//             Home
+//           </Link>
+//           <Link to="/helloworld" className="link">
+//             About
+//           </Link>
+//           <button onClick={this.handleLogin}>Login</button>
+//         </div>
+//         <div className="tabs">
+//           {error && <div>ERROR: {error}</div>}
+//           <Switch>
+//             <Route path="/" exact component={Home} />
+//             <ProtectedRoute
+//               path="/helloworld"
+//               loggedIn={this.state.loggedIn}
+//               component={HelloWorld}
+//             />
+//           </Switch>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default Header;
