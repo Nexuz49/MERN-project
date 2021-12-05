7b70245bce5a57fb1a50425f568ff5aad495aca6
Nexuz49 <58955897+Nexuz49@users.noreply.github.com>
2021-12-06 00:14:26 +0100

    Changes
    short desp.

--- client/app/components/About/About.js
+++ client/app/components/About/About.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const About = () => (
-  <p>Hello World!
-    This is a test project using the MERN Stack. // MongoDB, Express, React and Node 
-  </p>
+  <div>
+    <p>
+      This is a test project using the MERN Stack. // MongoDB, Express, React
+      and Node.
+    </p>
+    <p>
+      The main purpose with this project is to showcase an login and to build
+      and authentication method that actually works and can store information in
+      the DB. <br></br> 
+      User is kept logged in via Localstorage until the user logs out and token is done as a JWT.
+    </p>
+  </div>
 );
 
 export default About;
